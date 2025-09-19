e29873a4c38467a5862ca40d490bcb3f4922be68
breath57 <1498408920@qq.com>
2025-09-19 21:14:25 +0800

    fix: 升级langchainv0.3 后 current_time 工具需要定义参数类，否则会报错

--- imooc-llmops-api/internal/core/tools/builtin_tools/providers/time/current_time.py
+++ imooc-llmops-api/internal/core/tools/builtin_tools/providers/time/current_time.py
@@ -6,23 +6,29 @@
 @File    : current_time.py
 """
 from datetime import datetime
-from typing import Any, Type
+from typing import Any
 
 from pydantic import BaseModel
 from langchain_core.tools import BaseTool
 
+from internal.lib.helper import add_attribute
+
+
+class CurrentTimeInput(BaseModel):
+    """获取当前时间工具的输入参数（无需参数）"""
+    pass
 
 class CurrentTimeTool(BaseTool):
     """一个用于获取当前时间的工具"""
-    name: Type[str] = "current_time"
-    description: Type[str] = "一个用于获取当前时间的工具"
-    args_schema: Type[BaseModel] = BaseModel
+    name: str = "current_time"
+    description: str = "一个用于获取当前时间的工具，无需任何参数"
 
     def _run(self, *args: Any, **kwargs: Any) -> Any:
         """获取当前系统的时间并进行格式化后返回"""
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
 
 
+@add_attribute("args_schema", CurrentTimeInput)
 def current_time(**kwargs) -> BaseTool:
     """返回获取当前时间的LangChain工具"""
     return CurrentTimeTool()
